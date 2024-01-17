49d309791c49c8980fe29587be5e3648312381a6
김준연 <81897584+rntlls1366@users.noreply.github.com>
2024-01-18 02:22:59 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 584 ms, Memory: 96092 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int[] Ai = new int[A];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < A; i++) {
+            Ai[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[A];
+        int idx = 1;
+        dp[0] = Ai[0];
+        for(int i = 1; i < A; i++) {
+            if(dp[idx - 1] < Ai[i]) {
+                dp[idx] = Ai[i];
+                idx++;
+
+            }
+            else {
+                int point = binarySearch(dp, idx - 1, Ai[i]);
+                dp[point] = Ai[i];
+            }
+
+        }
+
+        System.out.println(idx);
+    }
+
+    static int binarySearch(int[] dp, int e, int n) {
+        int s = 0;
+        int mid = (s + e) / 2;
+        int answer = mid;
+        while(s < e) {
+            mid = (s + e) / 2;
+            if(n <= dp[mid]) {
+                answer = mid;
+                e = mid;
+            }
+            else {
+                s = mid + 1;
+                answer = s;
+            }
+        }
+
+        return answer;
+    }
+}
